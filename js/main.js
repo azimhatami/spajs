ecb146abca75fe0f62715fd4945bdacd6daefa25
azimhatami <azimhatami4@gmail.com>
2024-09-07 15:32:28 +0330

    Implement router function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
 const sidebar = document.getElementById("sidebar");
 const showBtn = document.querySelector('#btn')
 
+function router(params) {
+  const routes = [
+    {
+      path: '/',
+      view: () => console.log('dashboard page')
+    },
+    {
+      path: '/products',
+      view: () => console.log('products page')
+    },
+    {
+      path: '/posts',
+      view: () => console.log('posts page')
+    },
+  ];
+  
+  const potentialRoutes = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialRoutes.find((route) => route.isMatch);
+
+  if (!match) {
+    match = {
+      route : { 
+        path: '/not-found', 
+        view: () => console.log('not-found page')
+      },
+      isMatch: true
+    };
+  }
+  console.log(match.route.view());
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  router();
+})
+
+
+
+
+
 showBtn.addEventListener('click', toggleSidebar);
 
 function toggleSidebar() {
